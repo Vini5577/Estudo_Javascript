abffc170211b2eb2a23e94f4914a763f7cc4ce08
Hugo Vinícius <ugopessoa@gmail.com>
2022-11-23 10:49:09 -0300

    Finalizando Aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+
+while(c <= 100) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+c = 1
+do {
+
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 6)
+
+
+
+
+for(c = 0; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
