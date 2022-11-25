37c5ad4f2e4a1a9a6a4215c4d3264d6db1be5a03
Hugo Vinícius <ugopessoa@gmail.com>
2022-11-25 11:31:25 -0300

    falta pouco

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,7 +3,27 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+function isNumeri(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar(){
 
+    if(isNumero(num.value) && !inLista(num.value)) {
+        valores.push(Number(num.value))
+    } else {
+        window.alert('Valorinvalido ou já encontrado na lista.')
+    }
 }
